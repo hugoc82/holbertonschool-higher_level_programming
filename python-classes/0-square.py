33ca950d68627e3b79ff157c0f65ac4ea1b098f4
hugoc82 <hugocapelle@orange.fr>
2025-03-20 12:16:24 +0000

    .

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class called Square.
+The class does not have any attributes or methods.
+"""
 class Square:
+    """Represent a square."""
     pass
+
