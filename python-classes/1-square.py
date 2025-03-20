521389ffb30d78914bb224ce87df22419449aa40
hugoc82 <hugocapelle@orange.fr>
2025-03-20 12:40:04 +0000

    .

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
-Module Square : Ce module définit une classe Square avec un attribut privé size.
+Module Square : Ce module définit une classe Square avec un attribut privé 'size'.
 """
 
 class Square:
-    """Classe définissant un carré avec un attribut privé size."""
-    
+    """
+    Classe représentant un carré.
+
+    Elle contient un attribut privé 'size' pour la taille du côté du carré.
+    """
+
     def __init__(self, size):
         """
-        Initialise un objet Square avec un attribut privé size.
+        Initialise un objet Square avec un attribut privé 'size'.
 
-        Args:
-        size (int): La taille du côté du carré
+        Arguments :
+            size (int) : La taille du côté du carré.
         """
         self.__size = size
+
