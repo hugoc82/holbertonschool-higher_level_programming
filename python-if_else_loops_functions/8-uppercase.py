eaf3a69933deeff9963c9353211418587da656ac
hugoc82 <hugocapelle@orange.fr>
2025-03-20 09:20:19 +0000

    .

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = ""  # Une variable pour accumuler les résultats
+    result = ""
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
             result += chr(ord(c) - 32)
         else:
-            result += c  
-    print("{}".format(result))  
+            result += c
+    print("{}".format(result))
