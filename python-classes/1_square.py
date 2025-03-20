9bb936e97e126e3ff35aeb4317be2da6e4713dbb
hugoc82 <hugocapelle@orange.fr>
2025-03-20 12:23:03 +0000

    .

---
+++ python-classes/1_square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """A class that defines a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initialize the size of the square."""
+        self.__size = size  # Private instance attribute
