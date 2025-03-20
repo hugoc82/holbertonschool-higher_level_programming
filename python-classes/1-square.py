a9a5cd54deafd5c53de5cf93a13757ce4c4f2c6f
hugoc82 <hugocapelle@orange.fr>
2025-03-20 13:03:18 +0000

    .

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """
 Module pour définir une classe Square.
+La classe Square définit un carré avec un côté spécifique
+et permet de manipuler cet objet de manière simple.
 """
 
 class Square:
     """
     Représente un carré avec un côté donné par l'attribut 'size'.
+
+    Attributs:
+        size (int): La taille du côté du carré.
     """
 
     def __init__(self, size):
