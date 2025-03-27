9ae6f1fb5169c0d703b3800510176c2ade12e16a
hugoc82 <hugocapelle@orange.fr>
2025-03-27 14:41:45 +0000

    .

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- Ce script insère une nouvelle ligne dans la table 'first_table' 
+-- de la base de données 'hbtn_0c_0'.
+-- Les valeurs à insérer sont :
+-- id = 89 et name = 'Best School'.
+
+INSERT INTO first_table (id, name) 
+VALUES (89, 'Best School');
