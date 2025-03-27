ef80077386b56b256ad619e886810c1e03637300
hugoc82 <hugocapelle@orange.fr>
2025-03-27 13:37:57 +0000

    .

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,13 +1,18 @@
--- com
-SELECT
-    table_name AS Table,
-    GROUP_CONCAT(
-        CONCAT(
-            '  `', column_name, '` ', column_type, 
-            IF(is_nullable = 'NO', ' NOT NULL', ' NULL'),
-            IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), '')
-        ) ORDER BY ordinal_position SEPARATOR ',\n'
+SELECT 
+    CONCAT(
+        table_name, 
+        ' CREATE TABLE `', table_name, '` (',
+        GROUP_CONCAT(
+            CONCAT(
+                '\n  `', column_name, '` ', column_type,
+                IF(is_nullable = 'NO', ' NOT NULL', ' NULL'),
+                IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), ''),
+                IF(extra != '', CONCAT(' ', extra), '')
+            ) ORDER BY ordinal_position SEPARATOR ',',
+        ') ENGINE=', engine,
+        ' DEFAULT CHARSET=', table_collation
     ) AS 'Create Table'
-FROM information_schema.columns
-WHERE table_schema = DATABASE() AND table_name = 'first_table'
+FROM information_schema.tables t
+JOIN information_schema.columns c ON t.table_name = c.table_name
+WHERE t.table_schema = DATABASE() AND t.table_name = 'first_table'
 GROUP BY table_name;
