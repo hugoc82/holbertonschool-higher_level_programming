e6e37bd1447206b9e91ecb565941cb7e7c3e9573
hugoc82 <hugocapelle@orange.fr>
2025-03-27 14:09:19 +0000

    .

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,19 +1,16 @@
--- Ce script imprime la description complète de la table first_table
--- sans utiliser DESCRIBE ou EXPLAIN.
-
 SELECT 
     'first_table' AS table_name,
     'CREATE TABLE `first_table`(' AS create_statement,
     GROUP_CONCAT(
         CONCAT('`', COLUMN_NAME, '`', 
-            CASE WHEN IS_NULLABLE = 'NO' THEN ' NOT NULL' ELSE '' END, 
+            CASE WHEN IS_NULLABLE = 'NO' THEN 'NOT NULL' ELSE '' END, 
             CASE WHEN EXTRA != '' THEN ' ' + EXTRA ELSE '' END,
-            ' ', COLUMN_TYPE
+            COLUMN_TYPE
         )
-        SEPARATOR ', '
+        SEPARATOR ','
     ) AS table_columns,
     'PRIMARY KEY(`id`)' AS primary_key,
-    ')ENGINE=InnoDBDEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' AS table_end
+    ')ENGINE=InnoDBDEFAULT CHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci' AS table_end
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
 GROUP BY table_name;
