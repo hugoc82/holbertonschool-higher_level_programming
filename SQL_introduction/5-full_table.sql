1b99e6c1895a2dc3b1188995c63837be3a20ac84
hugoc82 <hugocapelle@orange.fr>
2025-03-27 13:42:58 +0000

    .

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,18 +1,20 @@
---com
-SELECT 
+-- This script prints the description of the table `first_table` in the current database.
+-- You are not allowed to use DESCRIBE or EXPLAIN.
+
+SELECT CONCAT(
+  table_name,
+  'CREATE TABLE `', table_name, '`(',
+  GROUP_CONCAT(
     CONCAT(
-        table_name, 
-        ' CREATE TABLE `', table_name, '` (',
-        GROUP_CONCAT(
-            CONCAT(
-                '\n  `', column_name, '` ', column_type,
-                IF(is_nullable = 'NO', ' NOT NULL', ' NULL'),
-                IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), ''),
-                IF(extra != '', CONCAT(' ', extra), '')
-            ) ORDER BY ordinal_position SEPARATOR ',',
-        ') ENGINE=', engine,
-        ' DEFAULT CHARSET=', table_collation
-    ) AS 'Create Table'
+      '`', column_name, '` ',
+      column_type,
+      IF(is_nullable = 'NO', ' NOT NULL', ' NULL'),
+      IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), ''),
+      IF(extra != '', CONCAT(' ', extra), '')
+    ) ORDER BY ordinal_position SEPARATOR ','),
+  ')ENGINE=', engine,
+  'DEFAULT CHARSET=', table_collation
+) AS 'Create Table'
 FROM information_schema.tables t
 JOIN information_schema.columns c ON t.table_name = c.table_name
 WHERE t.table_schema = DATABASE() AND t.table_name = 'first_table'
