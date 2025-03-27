27152e3207598ee00056a0f637d0d6fc762560f8
hugoc82 <hugocapelle@orange.fr>
2025-03-27 13:19:36 +0000

    .

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,12 @@
+SELECT
+    table_name AS Table,
+    GROUP_CONCAT(
+        CONCAT(
+            '  `', column_name, '` ', column_type, 
+            IF(is_nullable = 'NO', ' NOT NULL', ' NULL'),
+            IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), '')
+        ) ORDER BY ordinal_position SEPARATOR ',\n'
+    ) AS 'Create Table'
+FROM information_schema.columns
+WHERE table_schema = DATABASE() AND table_name = 'first_table'
+GROUP BY table_name;
