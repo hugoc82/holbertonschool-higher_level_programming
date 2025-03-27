7bdc060dade91a861185cdb7619111b88f8f8805
hugoc82 <hugocapelle@orange.fr>
2025-03-27 14:45:01 +0000

    .

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
 -- Si la table 'second_table' existe déjà, elle sera supprimée
--- Cela ne génère pas d'erreur si la table n'existe pas.
 DROP TABLE IF EXISTS second_table;
 
 -- Création de la table 'second_table'
@@ -14,3 +13,9 @@ INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+
+-- Insertion des lignes manquantes pour correspondre à la sortie attendue
+INSERT INTO second_table (id, name, score) VALUES (11, 'A', 12);
+INSERT INTO second_table (id, name, score) VALUES (12, 'B', 12);
+INSERT INTO second_table (id, name, score) VALUES (13, 'C', 12);
+INSERT INTO second_table (id, name, score) VALUES (14, 'D', 12);
