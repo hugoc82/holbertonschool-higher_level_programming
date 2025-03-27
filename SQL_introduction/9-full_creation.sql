69f33f96aa1cf220266ea5d528aede60223e8e5c
hugoc82 <hugocapelle@orange.fr>
2025-03-27 14:52:14 +0000

    .

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- Vérifier si la table 'second_table' existe déjà et la supprimer si c'est le cas
+-- Si la table 'second_table' existe déjà, elle sera supprimée
 DROP TABLE IF EXISTS second_table;
 
 -- Création de la table 'second_table'
@@ -8,17 +8,8 @@ CREATE TABLE second_table (
     score INT
 );
 
--- Insertion des 4 premières lignes
+-- Insertion des 4 premières lignes seulement
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
-
--- Ajout des nouveaux enregistrements après vérification de l'existence de la table
-INSERT INTO second_table (id, name, score) VALUES (11, 'A', 12);
-INSERT INTO second_table (id, name, score) VALUES (12, 'B', 12);
-INSERT INTO second_table (id, name, score) VALUES (13, 'C', 12);
-INSERT INTO second_table (id, name, score) VALUES (14, 'D', 12);
-
--- Affichage des enregistrements après ajout des nouveaux
-SELECT * FROM second_table;
