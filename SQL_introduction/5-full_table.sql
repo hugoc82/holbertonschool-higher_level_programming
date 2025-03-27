aaa9225349c8ff247f9d823592a1ebef164ed81e
hugoc82 <hugocapelle@orange.fr>
2025-03-27 14:08:07 +0000

    .

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,21 +1,19 @@
--- This script prints the description of the table `first_table` in the current database.
--- You are not allowed to use DESCRIBE or EXPLAIN.
+-- Ce script imprime la description complète de la table first_table
+-- sans utiliser DESCRIBE ou EXPLAIN.
 
-SELECT CONCAT(
-  table_name,
-  'CREATE TABLE `', table_name, '`(',
-  GROUP_CONCAT(
-    CONCAT(
-      '`', column_name, '` ',
-      column_type,
-      IF(is_nullable = 'NO', ' NOT NULL', ' NULL'),
-      IF(column_default IS NOT NULL, CONCAT(' DEFAULT ', column_default), ''),
-      IF(extra != '', CONCAT(' ', extra), '')
-    ) ORDER BY ordinal_position SEPARATOR ','),
-  ')ENGINE=', engine,
-  'DEFAULT CHARSET=', table_collation
-) AS 'Create Table'
-FROM information_schema.tables t
-JOIN information_schema.columns c ON t.table_name = c.table_name
-WHERE t.table_schema = DATABASE() AND t.table_name = 'first_table'
+SELECT 
+    'first_table' AS table_name,
+    'CREATE TABLE `first_table`(' AS create_statement,
+    GROUP_CONCAT(
+        CONCAT('`', COLUMN_NAME, '`', 
+            CASE WHEN IS_NULLABLE = 'NO' THEN ' NOT NULL' ELSE '' END, 
+            CASE WHEN EXTRA != '' THEN ' ' + EXTRA ELSE '' END,
+            ' ', COLUMN_TYPE
+        )
+        SEPARATOR ', '
+    ) AS table_columns,
+    'PRIMARY KEY(`id`)' AS primary_key,
+    ')ENGINE=InnoDBDEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' AS table_end
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
 GROUP BY table_name;
