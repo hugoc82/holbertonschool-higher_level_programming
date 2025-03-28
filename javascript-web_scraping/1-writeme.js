9dfc630eb5400ebd63f785107ea3da456dac89cb
hugoc82 <hugocapelle@orange.fr>
2025-03-28 14:02:59 +0000

    .

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+const fs = require('fs'); // Import the File System module
+
+// Get the file path and string to write from command line arguments
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+// Use fs.writeFile to write the string to the specified file
+fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
+  if (err) {
+    console.error(err); // If an error occurs, print it
+  }
+});
+
